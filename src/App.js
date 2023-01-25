89a7d03236604935b92ac26cb41bd9cf903f4e42
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2023-01-25 21:50:50 +0530

    Add Component and study import andusing this .

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import PartOne from './components/PartOne';
 
 function App() {
   return (
     <div className="App">
    <h1>This is my frist react project</h1>
+   <PartOne/>
+   <PartOne/>
+   <PartOne/>
     </div>
   );
 }
--- src/components/PartOne.js
+++ src/components/PartOne.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
 export default function PartOne() {
+    
   return (
     <div>
-        <h1>This is my Frist React Component</h1>
+        <h1>This is my Frist React Component...</h1>
     </div>
   )
 }
